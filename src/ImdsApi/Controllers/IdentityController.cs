41e08d10035a9e2d26f8caaa95d0b0ab4baba22b
Paul Wiens <paul@dysonshell.com>
2018-06-28 12:38:37 -0700

    Created initial project

---
+++ src/ImdsApi/Controllers/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImdsApi.Controllers
+{
+    [Route("metadata/[controller]")]
+    [ApiController]
+    [ApiVersion("2018-02-01")]
+    public class IdentityController : ControllerBase
+    {
+        // GET metadata/identity/oauth2/token
+        [HttpGet("oauth2/token")]
+        public IActionResult GetToken([FromQuery]string resource)
+        {
+            return Ok();
+        }
+    }
+}
